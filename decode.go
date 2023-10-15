683d76595f778adbb3e8d7b988f794af611c4301
Pedro Neri <pedrogneri@gmail.com>
2023-10-14 21:25:45 -0300

    feat: add errorAggregate

--- decode.go
+++ decode.go
@@ -2,21 +2,23 @@ package envelope
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"reflect"
+	"strings"
 )
 
 const (
-	envTag = "envelope"
+	structFieldTag = "envelope"
 )
 
 func Decode[T any](environmentModel *T) error {
 	refType := reflect.TypeOf(*environmentModel)
 
-	decodedMap, err := decodeStruct(refType)
-	if err != nil {
-		return err
+	decodedMap, errMsg := decodeStruct(refType)
+	if errMsg != "" {
+		return errors.New(errMsg)
 	}
 
 	marshal, err := json.Marshal(decodedMap)
@@ -30,40 +32,58 @@ func Decode[T any](environmentModel *T) error {
 	return nil
 }
 
-func decodeStruct(refType reflect.Type) (map[string]any, error) {
+func decodeStruct(refType reflect.Type) (map[string]any, string) {
 	fields := map[string]any{}
+	errorAggregate := []string{}
 
 	for i := 0; i < refType.NumField(); i++ {
 		field := refType.Field(i)
-		tagValue := field.Tag.Get(envTag)
+		tagValue, tagFound := field.Tag.Lookup(structFieldTag)
 
 		typeKind := field.Type.Kind()
 		if typeKind == reflect.Struct {
-			decoded, err := decodeStruct(field.Type)
-			if err != nil {
-				return nil, err
+			decoded, errMsg := decodeStruct(field.Type)
+			if errMsg != "" {
+				errorAggregate = append(errorAggregate, errMsg)
+				continue
 			}
 
-			for k, v := range decoded {
-				fields[k] = v
+			if field.Anonymous {
+				for k, v := range decoded {
+					fields[k] = v
+				}
+			} else {
+				fields[field.Name] = decoded
 			}
 
-			fields[field.Name] = decoded
+			continue
+		}
+
+		if !tagFound {
 			continue
 		}
 
 		fieldProps := getFieldProperties(tagValue)
-		value, ok := os.LookupEnv(fieldProps.Key)
-		if !ok && fieldProps.Required {
-			return nil, fmt.Errorf(`missing a required field "%s"`, fieldProps.Key)
+		value, foundEnv := os.LookupEnv(fieldProps.Key)
+		if !foundEnv && fieldProps.Required {
+			errMsg := fmt.Sprintf(`missing a required field "%s"`, fieldProps.Key)
+			errorAggregate = append(errorAggregate, errMsg)
+			continue
 		}
 
 		convertedValue, err := convert(typeKind, value)
 		if err != nil {
-			return nil, err
+			errMsg := fmt.Sprintf(`error converting value from "%s" field into %s"`, fieldProps.Key, typeKind)
+			errorAggregate = append(errorAggregate, errMsg)
+			continue
 		}
 
 		fields[field.Name] = convertedValue
 	}
-	return fields, nil
+
+	if len(errorAggregate) > 0 {
+		return nil, strings.Join(errorAggregate, "; ")
+	}
+
+	return fields, ""
 }
